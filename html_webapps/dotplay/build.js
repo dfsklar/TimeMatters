2350d94bea6a900319642270a1bf164ded6a5f36
David Sklar <dfsklar@gmail.com>
2016-09-21 20:16:57 -0400

    done

--- html_webapps/dotplay/build.js
+++ html_webapps/dotplay/build.js
@@ -7,31 +7,44 @@ function getRandomIntInclusive(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
+// Returns a random integer between min (included) and max (included)
+// Using Math.round() will give you a non-uniform distribution!
+function getRandomFloatInclusive(min, max) {
+  return (Math.random() * (max - min + 1)) + min;
+}
+
+
+
 $(document).ready(function() {
     console.log('hello');
     // Ready to construct the boids
     $slate = $('.slate');
-    fullAnimDuration = 8;
     for (var i=0; i < 50; i++) {
-        left = getRandomIntInclusive(10,200);
-        width = getRandomIntInclusive(5,10);
-        height = getRandomIntInclusive(5,10);
-        animDelay = getRandomIntInclusive(0, 7);
+        fullAnimDuration = getRandomFloatInclusive(2,3);
+        X = getRandomIntInclusive(10,400);
+        Y = getRandomIntInclusive(10,30);
+        width = getRandomIntInclusive(5,15);
+        height = getRandomIntInclusive(5,13);
+        animDelay = getRandomFloatInclusive(0, fullAnimDuration);
+
+        // . c i r c l e  (the innermost)
         $inner = $(`<div class="circle" id="cc${i}"></div></div>`);
         $inner.css({
             backgroundColor: 'blue',
             height: `${height}px`,
             width: `${width}px`,
-            transformOrigin: 'center 300%',
-            animationDuration: '7s',
-            animationDelay: `${animDelay}s`
+            animationDuration: `${fullAnimDuration}s`,
+            animationDelay: `${animDelay+(fullAnimDuration/4)}s`
         });
+
         $outer = $(`<div class="circle-outer" id="c${i}"></div>`);
         $outer.css({
             position: 'absolute',
-            left: `${left}px`,
-            animation: "updown 4s infinite ease-in-out alternate"
+            left: `${X}px`,
+            animation: `updown ${fullAnimDuration/2.0}s infinite ease-in-out alternate`,
+            animationDelay: `${animDelay}s`
         });
+
         $inner.appendTo($outer);
         $outer.appendTo($slate);
     }
