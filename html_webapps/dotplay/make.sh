fcbb80c56484d41cd7c0a3eda6088115f95bb365
root <root@li479-42.members.linode.com>
2016-10-01 23:26:34 -0400

    First version loadable by android phone

--- html_webapps/dotplay/make.sh
+++ html_webapps/dotplay/make.sh
@@ -2,6 +2,6 @@ D=../../deploy
 mkdir -p $D
 cp * $D
 OPT=" --presets latest "
-babel $OPT --out-file $D/_atoms.js < atoms.js
-babel $OPT --out-file $D/_build.js < build.js
-babel $OPT --out-file $D/_timeline.yaml < timeline.yaml
+babel $OPT --out-file $D/atoms.js < atoms.js
+babel $OPT --out-file $D/build.js < build.js
+babel $OPT --out-file $D/timeline.yaml < timeline.yaml
