bc967e0dd8ed9b3c98544029c7759f0e71abc505
dfsklar <dfsklar@gmail.com>
2016-10-03 00:47:10 -0230

    FEW

--- src/atoms.js
+++ src/atoms.js
@@ -46,8 +46,7 @@ function build_particles(Wmin, Wmax, qty, Xleft, Xdens, Xright, YmaxL, YmaxR) {
         var Y = Ymax;   //(Ymax > 0) ? getRandomFloatInclusive(0, Ymax) : 10;
         var width = getRandomFloatInclusive(Wmin, Wmax);
         var height = width;
-        var Y_int = Math.abs(Math.round(Y));
-        var rate_of_speed = (getRandomFloatInclusive(1, 1.5) + Math.pow(2, -Y_int)) * 180;
+        var fullAnimDuration = Math.min(Ymax / 80, 3) * getRandomFloatInclusive(0.99,1.01);
         if (false) {
             console.log('------');
             console.log(Xleft);
@@ -62,11 +61,10 @@ function build_particles(Wmin, Wmax, qty, Xleft, Xdens, Xright, YmaxL, YmaxR) {
             console.log(Y_int);
             console.log(rate_of_speed);
         }
-        var fullAnimDuration = (2*Y_int) / rate_of_speed;
         var width = 2 * Math.round(getRandomFloatInclusive(Wmin/2.0, Wmax/2.0)); // must be even number to avoid "clipped-circle" look
         var height = width;
         var animDelay = 0; //getRandomFloatInclusive(0, fullAnimDuration);
-        var animStyle = Math.min(window.Ymax, getRandomIntInclusive(1, Y_int));
+        var animStyle = Math.min(window.Ymax, Math.round(Y));
 
         var $inner = $(`<div class="circle" id="cc${i}"></div></div>`);
         $inner.css({
@@ -78,7 +76,7 @@ function build_particles(Wmin, Wmax, qty, Xleft, Xdens, Xright, YmaxL, YmaxR) {
             left: `${X-width/2}px`,
             top: `${0-height/2}px`,
             animation: `cycle${animStyle} ${fullAnimDuration}s infinite alternate ease-in-out`,
-            animationDelay: `${animDelay+(fullAnimDuration/2)}s`
+            animationDelay: `${animDelay}s`
         });
         $inner.appendTo($slate);
     }
