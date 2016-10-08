89a10b83fd4fdefe69ed1762777e075059cfeafa
dfsklar <dfsklar@gmail.com>
2016-10-08 18:09:18 -0300

    leaves are not showing up on iphone old

--- src/post-stylie.sh
+++ src/post-stylie.sh
@@ -1,3 +1,3 @@
 python post-stylie.py < stylie-export-1.txt > stylie-export-1.keyframes
 python post-stylie.py < stylie-export-2.txt > stylie-export-2.keyframes
-python post-stylie.py < stylie-export-3.txt > stylie-export-3.keyframes
+# python post-stylie.py < stylie-export-3.txt > stylie-export-3.keyframes
